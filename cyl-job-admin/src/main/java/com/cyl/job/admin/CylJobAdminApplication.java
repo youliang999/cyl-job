b7be37d3147c086a8761c41097bea8b8e4d531bc
chengyouliang <1072223954@qq.com>
2019-07-02 23:14:36 +0800

    初始化cyl-job

---
+++ cyl-job-admin/src/main/java/com/cyl/job/admin/CylJobAdminApplication.java
@@ -0,0 +1,13 @@
+package com.cyl.job.admin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CylJobAdminApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CylJobAdminApplication.class, args);
+	}
+
+}
---
+++ cyl-job-admin/src/main/java/com/cyl/job/admin/trigger/JobTrigger.java
@@ -0,0 +1,14 @@
+package com.cyl.job.admin.trigger;
+
+
+import com.cyl.job.admin.enums.TriggerTypeEnum;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class JobTrigger {
+    private static Logger log = LoggerFactory.getLogger(JobTrigger.class);
+
+    public static void trigger(int jobId, TriggerTypeEnum triggerTypeEnum, int failRetryCount, String executorParam) {
+
+    }
+}
